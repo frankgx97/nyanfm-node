f288fce91dda203d05851d6806f9700ca24a4a52
frank <i@nyan.im>
2017-04-20 21:59:11 +0800

    delete unnecessary files

--- models/song.js
+++ models/song.js
@@ -2,12 +2,14 @@ var sql_connect = require('../conf.js');
 module.exports = {
     schema: {
         id: { type: 'serial', key: true },
+        song_id: String,
         title: String,
         artist: String,
         album: String,
         cover: String,
         mp3: String,
         ogg: String,
-        lyric: String
+        lyric: String,
+        update_at: { type: 'date', null: true }
     }
 }
