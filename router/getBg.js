9cfb725156de478e5fc3e74b09efaa55f03bcd99
frank <i@nyan.im>
2017-04-22 15:29:05 +0800

    添加!nyanfm.bg后缀

--- router/getBg.js
+++ router/getBg.js
@@ -7,7 +7,7 @@ router.get("/", (req, res) => {
             throw err;
         var rst = []
         for (var i = 0; i < 20; i++) {
-            rst.push(bg[Math.floor((Math.random() * bg.length - 1) + 0)]);
+            rst.push(bg[Math.floor((Math.random() * bg.length - 1) + 0)].url + '!nyanfm.bg');
         }
         res.send(rst);
     });
