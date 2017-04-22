2965303fbfd753590214b9f1f43bd329d093624d
frank <i@nyan.im>
2017-04-22 15:15:03 +0800

    修改背景图加载逻辑，升级backstrech

--- public/js/background.js
+++ public/js/background.js
@@ -1,22 +1,14 @@
- var promise = new Promise(function(resolve, reject) {
-     $.ajax({
-         url: '/get_bg',
-         type: 'get',
-         success: function(data) {
-             var rst = [];
-             data.forEach(function(element) {
-                 rst.push(element.url);
-             }, this);
-             resolve(rst);
-         },
-         fail: function(err) {
-             alert(err);
-         }
-     });
- });
- promise.then(function(data) {
-     $.backstretch([data], {
-         duration: 5000,
-         fade: 1200
-     });
- });
+$.ajax({
+    url: '/get_bg',
+    type: 'get',
+    success: function(data) {
+        console.log(data)
+        $.backstretch([data], {
+            duration: 5000,
+            fade: 1200
+        });
+    },
+    fail: function(err) {
+        alert(err);
+    }
+});
