4953e8c1e9b36d9f409af255264e635db58ab8fd
frank <i@nyan.im>
2017-04-22 15:59:32 +0800

    bugfix

--- public/js/background.js
+++ public/js/background.js
@@ -1,14 +1,16 @@
-$.ajax({
-    url: '/get_bg',
-    type: 'get',
-    success: function(data) {
-        console.log(data)
-        $.backstretch([data], {
-            duration: 5000,
-            fade: 1200
-        });
-    },
-    fail: function(err) {
-        alert(err);
-    }
+$(document).ready(function() {
+    $.ajax({
+        url: '/get_bg',
+        type: 'get',
+        success: function(data) {
+            console.log(data)
+            $.backstretch(data, {
+                duration: 5000,
+                fade: 1200
+            });
+        },
+        fail: function(err) {
+            alert(err);
+        }
+    });
 });
