3509b2d06678e9cf80bdf7ab872476bdb7353e7a
frank <i@nyan.im>
2018-03-15 23:00:38 +0800

    legacy frontend

---
+++ static_legacy/js/background.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+    $.ajax({
+        url: api_url + '/get_bg',
+        type: 'get',
+        success: function(data) {
+            //console.log(data)
+            $.backstretch(data, {
+                duration: 5000,
+                fade: 1200
+            });
+        },
+        fail: function(err) {
+            alert(err);
+        }
+    });
+});
