1a3112b54a4b7192476a975b9398f2e453e36ff3
frank <i@nyan.im>
2018-03-15 11:09:07 +0800

    Finish getList API

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
 #coding:utf8
 from flask import Flask, jsonify
 from database import db, Song, Bg 
+from playhouse.shortcuts import model_to_dict
 
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='static',static_url_path='')
 
-prefix = '/api'
+prefix = '/api/'
 
 @app.route('/')
 def index():
     '''
     Static front page
     '''
-    return app.send_static_file('/static/index.html')
+    return app.send_static_file('index.html')
 
 @app.route(prefix + 'get_list')
 def getList():
     '''
     Return playlist
     '''
-    playlist = Song.select().get()
-    return jsonify(playlist)
+    playlist = Song.select().order_by(Song.id.desc()).dicts()
+    return jsonify(list(playlist))
 
 @app.route(prefix + 'get_bg')
 def getBg():
